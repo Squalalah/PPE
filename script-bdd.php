6aeeaa288df91396f4e317a7c665cebf6eab0631
Squalalah <riquezoune@gmail.com>
2016-03-20 18:06:48 +0100

    edit ligne
    suppression d'une ligne

--- script-bdd.php
+++ script-bdd.php
@@ -9,7 +9,6 @@
 
 	for ($i = 0; $i < 5;$i++)
 	{
-
 		$code = array(rand(1000,9999),rand(1000,9999),rand(1000,9999),rand(1000,9999));
 		echo $code[0] . "-" . $code[1] . "-" . $code[2] . "-" . $code[3] . "<br />";
 	}

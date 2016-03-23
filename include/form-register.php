cb5cffc09a037f671bcd0867d8d8e50414e06110
Squalalah <riquezoune@gmail.com>
2016-03-23 12:30:49 +0100

    Retrait code produit
    Inutile, on pourra s'inscrire sans produit

--- include/form-register.php
+++ include/form-register.php
@@ -14,13 +14,6 @@
     					<label for="ins-password">Mot de passe : </label><input type="text" title="ins" name="ins-password" placeholder="Mot de passe choisi" required /><br />
     					<label for="insc-password">Confirmer mot de passe : </label><input type="text" title="ins" name="insc-password" required /><br />
     					<label for="ins-mail">Adresse Mail : </label><input type="email" title="ins" name="ins-mail" required /><br />
-    					<!--<label for="code">Code Produit : </label><input type="text" name="code" placeholder="Code produit donné lors de l'achat du produit" required /><br /> -->
-    					
-    					<label for="code1">Code produit :</label>
-    					<input class="code" type="text" name="code1" placeholder="XXXX" maxlength="4" minlength="4">
-    					<input class="code" type="text" name="code2" placeholder="XXXX" maxlength="4" minlength="4">
-    					<input class="code" type="text" name="code3" placeholder="XXXX" maxlength="4" minlength="4">
-    					<input class="code" type="text" name="code4" placeholder="XXXX" maxlength="4" minlength="4">
     					<p>En vous inscrivant, vous allez lu et approuvé nos <a href="#">Conditions Générales d'Utilisation</a>.</p><br />
     					<input type="submit" id="submit" name="ins-submit" value="Valider" />
 

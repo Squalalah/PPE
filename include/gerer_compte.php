9a996b03cf3ef28d097d038f6bd438c63018e4da
Squalalah <riquezoune@gmail.com>
2016-03-24 21:58:54 +0100

    Ajout compte, gere_compte.php et compte.css

---
+++ include/gerer_compte.php
@@ -0,0 +1,50 @@
+
+
+<?php
+
+
+	public function afficherGererCompte()
+	{
+
+		?>
+
+
+		<div class="parent">
+
+			<div class="enfant">
+				Element 1
+			</div>
+
+			<div class="enfant">
+				Element 2
+			</div>
+
+			<div class="enfant">
+				Element 3
+			</div>
+
+
+		</div>
+
+		<div class="parent">
+
+			<div class="enfant">
+				Element 4
+			</div>
+
+			<div class="enfant">
+				Element 5
+			</div>
+
+			<div class="enfant">
+				Element 6
+			</div>
+
+		</div>
+
+
+
+
+	}
+
+?>

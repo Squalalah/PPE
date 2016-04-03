de9b9a7ac7b26a4e3387b6edf2b0328883e1a379
Squalalah <riquezoune@gmail.com>
2016-04-03 19:21:11 +0200

    ajout - gestion des messages
    lorsque l'on clique sur le numéro de ticket, on a accès aux messages
    liés au ticket.

--- include/gerer_compte.php
+++ include/gerer_compte.php
@@ -4,6 +4,11 @@
 	
 	function afficherGererCompte()
 	{
+
+		require_once 'connexion.class.php';
+      	$ConnexionBaseSIO = new Connexion();
+        $IDconnexion = $ConnexionBaseSIO->IDconnexion;
+        $IDmessage = $ConnexionBaseSIO->IDmessage;
 		?>
 
 
@@ -24,13 +29,13 @@
 					<p>Paramètres</p>[CONSTRUCTION]</a>
 				</div>
 <?php
-					if(isset($_GET['ticket']))
+					
+					if(isset($_GET['ticket']) AND !isset($_GET['contenu']))
 					{
+
                 		if($_GET['ticket'] == $_SESSION['id'])
 						{
-							require_once 'connexion.class.php';
-                			$ConnexionBaseSIO = new Connexion();
-                			$IDconnexion = $ConnexionBaseSIO->IDconnexion;
+							
                 			$ticket = $IDconnexion->prepare("SELECT id_ticket, desc_ticket, etat_ticket, EXTRACT(YEAR FROM date_ticket) AS AAAA,
 							EXTRACT(MONTH FROM date_ticket) AS MM,
 							EXTRACT(DAY FROM date_ticket) AS JJ, id_client FROM ticket WHERE id_client = :id_client");
@@ -47,14 +52,16 @@
 											<th>Crée le :</th>
 										</tr>
 
-<?php 									if($sql > 0)
+<?php									if($sql > 0)
 										{ 
 											foreach($ticket as $donnees):
 												if($donnees['etat_ticket'] == 0) $etat = "Fermé";
 												else $etat = "Ouvert";
-												$date = $donnees['JJ'] . "/" . $donnees['MM'] . "/" . $donnees['AAAA']; ?>
+												$date = $donnees['JJ'] . "/" . $donnees['MM'] . "/" . $donnees['AAAA']; 
+												?>
 												<tr>
-													<td><?php echo $donnees['id_ticket']; ?></td>
+													<td><a href=<?php echo "?contenu=" . $donnees['id_ticket'];?>>
+														<?php echo $donnees['id_ticket'];?> </a></td>
 													<td><?php echo $donnees['desc_ticket']; ?></td>
 													<td><?php echo $etat; ?></td>
 													<td><?php echo $date; ?></td>
@@ -63,10 +70,49 @@
 										} ?>
 									</table>
 								</div>
-<?php					}
+<?php					$ticket->CloseCursor();
+						}
 						else header('Location: compte.php');?>
 						</div>
-<?php        		}?>
+<?php        		}
+					else if (isset($_GET['contenu']) AND !isset($_GET['ticket']))
+					{
+
+							// A rajouter, moyen de sécurité pour éviter qu'une autre personne lise les messages d'une autre personne.
+							// Solution : reussir à implanter deux GET dans l'URL quand on clique sur le numéro de ticket
+							$contenu = $_GET['contenu'];
+							$message = $IDconnexion->prepare("SELECT * FROM message WHERE id_ticket = :id_ticket ORDER BY date_message");
+                			$message->bindValue('id_ticket', $contenu, PDO::PARAM_INT);
+                			$message->execute();
+                			$squa = $message->rowCount();
+                			 ?>
+
+                			 <div class="enfant">
+								<div id="tableau">
+									<table>
+										<tr>
+											<th>N°Message</th>
+											<th>Auteur</th>
+											<th>Contenu</th>
+											<th>Envoyé le :</th>
+										</tr>
+
+<?php									if($squa > 0)
+										{ 
+											foreach($message as $data):
+												?>
+												<tr>
+													<td><?php echo $data['id_message'];?> </a></td>
+													<td><?php echo $data['auteur_message']; ?></td>
+													<td><?php echo $data['contenu_message']; ?></td>
+													<td><?php echo $data['date_message']; ?></td>
+												</tr>
+<?php 										endforeach;
+										} ?>
+									</table>
+								</div>
+							</div>
+<?php					 }?>
 				</div>
 			</div>
 <?php
